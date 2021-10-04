fb8491e47f2b41a6817ea67f2b475b2f797db761
Ben Kaan <benkaan001@gmail.com>
2021-10-03 23:07:53 -0500

    fs test

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,14 @@
 // TODO: Include packages needed for this application
 
+const fs = require('fs');
+
+const generateMarkdown = require('./utils/generateMarkdown');
+
+
 // TODO: Create an array of questions for user input
 const questions = [];
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
